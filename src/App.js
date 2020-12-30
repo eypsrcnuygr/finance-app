65c02c0b720fd81333436fd246d18561ec43d009
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-12-31 02:15:50 +0300

    Try to make the app work on refresh

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react';
 import { REACT_APP_API_TOKEN, apiUrl } from './helpers/variables';
 
 function App() {
-  const [data, setData] = useState({});
-
+  const realData = [];
   async function getReq() {
     try {
       const response = await fetch(
         `${apiUrl}/api/v3/profile/AAPL?apikey=${REACT_APP_API_TOKEN}`,
       );
       const data = await response.json();
-      return setData(data);
+      return data;
     } catch (e) {
       return e;
     }
   }
 
-  useEffect(() => {
-    getReq();
-  }, []);
+  (async function caller() {
+    const data = await getReq();
+    realData.push(data);
+  }());
+  console.log(realData);
 
   return (
-    <div>{data[0].description}</div>
+    <div>{realData}</div>
   );
 }
 
