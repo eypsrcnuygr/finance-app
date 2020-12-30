f8da7798b29dd310dd173b4ddef8e97aadb45647
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2020-12-31 01:50:04 +0300

    Add variables.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
+import { REACT_APP_API_TOKEN, apiUrl } from './helpers/variables';
 
 function App() {
   const [data, setData] = useState({});
-
-  const { REACT_APP_API_TOKEN } = process.env;
-  const apiUrl = 'https://financialmodelingprep.com/';
 
   async function getReq() {
     try {
---
+++ src/helpers/variables.js
@@ -0,0 +1,2 @@
+export const { REACT_APP_API_TOKEN } = process.env;
+export const apiUrl = 'https://financialmodelingprep.com/';
